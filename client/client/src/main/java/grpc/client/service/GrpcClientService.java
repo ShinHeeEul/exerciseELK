f17419f67d771d7b17c629f816ce2c0c3a630b92
ShinHeeEul <sheshe7015@naver.com>
2025-02-13 13:47:28 +0900

    feat: GrpcClientService.java - Client Stream 방식으로 요청 보내는 서비스 로직 작성

---
+++ client/client/src/main/java/grpc/client/service/GrpcClientService.java
@@ -0,0 +1,61 @@
+package grpc.client.service;
+
+import com.google.protobuf.Timestamp;
+import grpc.proto.IotLogProto;
+import grpc.proto.IotLogServiceGrpc;
+import io.grpc.stub.StreamObserver;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class GrpcClientService {
+
+    private final IotLogServiceGrpc.IotLogServiceStub iotLogServiceStub;
+
+    public void test() {
+            StreamObserver<IotLogProto.LogRequest> requestObserver = iotLogServiceStub.streamLogs(new StreamObserver<>() {
+                @Override
+                public void onNext(IotLogProto.LogResponse logResponse) {
+                    log.info(logResponse.toString());
+                }
+
+                @Override
+                public void onError(Throwable t) {
+                    log.error(t.toString());
+                    t.printStackTrace();
+
+                }
+
+                @Override
+                public void onCompleted() {
+                    log.info("completed");
+                }
+            });
+
+            for(int i = 0; i < 10; i++) {
+                requestObserver.onNext(generateLogRequest(i));
+            }
+            requestObserver.onCompleted();
+    }
+
+    public static IotLogProto.LogRequest generateLogRequest(int number) {
+
+        Instant now = Instant.now();
+        Timestamp timestamp = Timestamp.newBuilder()
+                .setSeconds(now.getEpochSecond())
+                .setNanos(now.getNano())
+                .build();
+        return IotLogProto.LogRequest.newBuilder()
+                .setMessage("로그 메세지입니다. : " + number)
+                .setLogLevel(IotLogProto.LogLevel.INFO)
+                .setDeviceId(number + "")
+                .setTimestamp(timestamp)
+                .build();
+    }
+
+}
